bc86b6181c703dbe0509073759758df33b970e09
elenakost <elenakostandin@gmail.com>
2018-01-10 08:21:26 -0500

    Update dao.py

--- dao.py
+++ dao.py
@@ -1,34 +1,30 @@
 from pymongo import MongoClient
+from pymongo import ObjectId
 
 class Dao:
     def __init__(self):
-        table = '' 
-        mongo = MongoClient('mongo')
-        client = getattr(mongo, table)
-        self.db = client
-	      self.collection=self.db['students']
+         connection = MongoClient('localhost', 27017)
+         self.db = self.connection['test']
+         self.collection = self.db['students']
 
     def get_by_id(self, student_Id):
-        try:
-       	    student= self.collection.find_one({'_id': ObjectId(student_Id)})
-        except:
-            raise
-	      if not student:
-		       return None
-        return student
+       	student= self.collection.find_one({'_id': ObjectId(student_Id)})
+	if student is not None:
+		return student
+        return None
 		
     def getStudents(self):
         return self.collection
 
     def delete(self, student_Id):
-        try:
-		        deleted = self.collection.delete_one({'_id': ObjectId(student_Id)})
-            student = self.collection.find_one({'_id': ObjectId(student_Id)})
-        except:
-		        raise
+        student = self.collection.find_one({'_id:': ObjectId(student_Id)})
         if not student:
-            return None
-	      return deleted
+            	return None
+        try:
+		deleted = self.collection.delete_one({"_id": ObjectId(student_Id)})
+        except:
+		raise
+	return deleted
 
     def deleteAll(self):
         self.collection.remove({})
@@ -42,17 +38,15 @@ class Dao:
             raise
         if not student:
             return None
-        else:
-            return student
+        return student
 
     def addStudent(self, studentData):
         try:
             Id = self.collection.insert_one(studentData).inserted_id
         except:
             raise
-        if not Id:
-            return None
-        return self.collection.find_one({"_id": Id.inserted_id})
-
+        if Id is not None:
+            return self.collection.find_one({"_id": Id.inserted_id})
+        return None
 
  
