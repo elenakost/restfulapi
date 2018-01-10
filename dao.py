75cc20396ddc62abc8e914e3cb23a694830c0244
elenakost <elenakostandin@gmail.com>
2018-01-10 14:39:57 -0500

    Update dao.py

--- dao.py
+++ dao.py
@@ -1,52 +1,53 @@
 from pymongo import MongoClient
-from pymongo import ObjectId
+from bson.objectid import ObjectId
 
 class Dao:
-    def __init__(self):
-         connection = MongoClient('localhost', 27017)
-         self.db = self.connection['test']
-         self.collection = self.db['students']
+	def __init__(self):
+		connection = MongoClient('localhost', 27017)
+		self.db = connection['test']
+		self.collection = self.db['students']
 
-    def get_by_id(self, student_Id):
-       	student= self.collection.find_one({'_id': ObjectId(student_Id)})
-	if student is not None:
+	def get_by_id(self, student_Id):
+		student= self.collection.find_one({'_id': ObjectId(student_Id)})
+		if student is not None:
+			return student
+		return None
+
+	def getStudents(self):
+		return self.collection
+
+	def delete(self, student_Id):
+		student = self.collection.find_one({'_id:': ObjectId(student_Id)})
+		if not student:
+			return None
+		try:
+			deleted = self.collection.delete_one({"_id": ObjectId(student_Id)})
+		except:
+			raise
+		return deleted
+    
+	def deleteAll(self):
+        	self.collection.remove({})
+        	return
+
+	def update(self, student_Id, studentData):
+		try:
+			updatedStudent = self.collection.update_one({"_id": ObjectId(student_Id)}, {"$set": studentData}, upsert = False)
+			student = self.collection.find_one({'_id': ObjectId(student_Id)})
+		except:
+			raise
+		if not student:
+			return None
 		return student
-        return None
-		
-    def getStudents(self):
-        return self.collection
 
-    def delete(self, student_Id):
-        student = self.collection.find_one({'_id:': ObjectId(student_Id)})
-        if not student:
-            	return None
-        try:
-		deleted = self.collection.delete_one({"_id": ObjectId(student_Id)})
-        except:
-		raise
-	return deleted
-
-    def deleteAll(self):
-        self.collection.remove({})
-        return
-
-    def update(self, student_Id, studentData):
-        try:
-            updatedStudent = self.collection.update_one({"_id": ObjectId(student_Id)}, {"$set": studentData}, upsert = False)
-            student = self.collection.find_one({'_id': ObjectId(student_Id)})
-        except:
-            raise
-        if not student:
-            return None
-        return student
-
-    def addStudent(self, studentData):
-        try:
-            Id = self.collection.insert_one(studentData).inserted_id
-        except:
-            raise
-        if Id is not None:
-            return self.collection.find_one({"_id": Id.inserted_id})
-        return None
+	def addStudent(self, studentData):
+		try:
+			Id = self.collection.insert_one(studentData).inserted_id
+		except:
+			raise
+		if Id is not None:
+			return self.collection.find_one({"_id": Id.inserted_id})
+		return None
 
  
+
