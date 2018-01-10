e25f3b7e6db2a7a15986593a61d4d48e68c8b6f1
elenakost <elenakostandin@gmail.com>
2018-01-10 13:12:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,55 +1,109 @@
-from flask import Flask, request
-from flask_restful import reqparse, abort, Api, Resource
-from flask_pymongo import PyMongo
+from flask import Flask, jsonify
+from flask_restful import Resource, Api, reqparse, request
+from model import Model
+from voluptuous.voluptuous.schema_builder import Schema, Required
+from voluptuous.voluptuous.validators import Length, All, MultipleInvalid
 
 app = Flask(__name__)
-mongo = PyMongo(app)
 api = Api(app)
-
-
-USERS = {
-    'user1': {'name': 'elena'},
-    'user2': {'name': 'bob'},
-}
-
-def abort_if_user_doesnt_exist(user_id):
-    if user_id not in USERS:
-        abort(404, message="User {} doesn't exist".format(user_id))
 
 parser = reqparse.RequestParser()
 parser.add_argument('name')
 
-class Users(Resource):
-    def get(self, user_id):
-        abort_if_user_doesnt_exist(user_id)
-        return USERS[user_id]
+idSchema = Schema(All(str, Length(min=24, max=24)))
+jsSchema=Schema(str)
 
-    def delete(self, user_id):
-        abort_if_user_doesnt_exist(user_id)
-        del USERS[user_id]
+class Student(Resource):
+    def get(self, student_id):
+        try:
+            if(idSchema(student_id)==student_id):
+                student=Model.get_by_id(student_id)
+            else:
+                raise AssertionError('MultipleInvalid not raised')
+        except MultipleInvalid as e:
+            exc = e
+            print (str(exc))
+        if student is None:
+            abort(404, message="Getting Student {} failed".format(student_id))
+        return student
+        
+    def delete(self, student_id):
+        try:
+            if(idSchema(student_id)==student_id):
+                deleted = Model.delete(student_Id)
+            else:
+                raise AssertionError('MultipleInvalid not raised')           
+        except MultipleInvalid as e:
+            exc = e
+            print (str(exc))
+        if student is None:
+            abort(404, message="Deleting Student {} failed".format(student_id))
         return '', 204
-
-    def put(self, user_id):
-        args = parser.parse_args()
-        n= {'name': args['name']}
-        USERS[user_id] = n
-        return n, 201
-
-class UsersList(Resource):
-    def get(self):
-        return USERS
-    #not working, telling me post is an invalid method  
+        
     def post(self):
         args = parser.parse_args()
-        user = int(max(USERS.keys()).lstrip('user')) + 1
-        user_id = 'user%(user)' % {"user"}
-        USERS[user_id] = {'name': args['name']}
-        return USERS[user_id], 201
+        try:
+            if(jsSchema(args['name'])==args['name']):
+                n= {'name': args['name']}
+                student = Model.addStudent(n)
+            else:
+                raise AssertionError('MultipleInvalid not raised')           
+        except MultipleInvalid as e:
+            exc = e
+            print (str(exc))
+        return student, 201
+        
+    def put(self, student_id):
+        args = parser.parse_args()
+        try:
+            if(jsSchema(args['name'])==args['name'] && idSchema(student_id)==student_id)):
+                n= {'name': args['name']}
+                student = Model.update(student_Id, n)
+            else:
+                raise AssertionError('MultipleInvalid not raised')           
+        except MultipleInvalid as e:
+            exc = e
+            print (str(exc))
+        return student, 201
+    
+class StudentList(Resource):
+    def get(self):
+        try:
+            students = Model.getStudents() 
+        except:
+            print("an error")
+        if students is None:
+            abort(404, message="Getting all Students {} failed".format(student_id))
+        return students
+        
+    def post(self):
+        args = parser.parse_args()
+        try:
+            if(jsSchema(args['name'])==args['name']):
+                n= {'name': args['name']}
+                student = Model.addStudent(n)
+            else:
+                raise AssertionError('MultipleInvalid not raised')           
+        except MultipleInvalid as e:
+            exc = e
+            print (str(exc))
+        return student, 201
 
-#not sure what these 2 lines do
-api.add_resource(UsersList, '/users')
-api.add_resource(Users, '/users/<user_id>')
+     def delete(self, student_id):
+        try:
+            if(idSchema(student_id)==student_id):
+                deleted = Model.deleteAll(student_Id)
+            else:
+                raise AssertionError('MultipleInvalid not raised')
+        except MultipleInvalid as e:
+            exc = e
+            print (str(exc))
+        if deleted is None:
+            abort(404, message="Deleting Students {} failed".format(student_id))
+        return '', 204
 
+api.add_resource(StudentList, '/students')
+api.add_resource(Students, '/students/<student_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
