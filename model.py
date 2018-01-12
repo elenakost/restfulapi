502d6b46830292009bc161b754d565aec80bc6e2
ElenaKostandin <elenakostandin@admins-mbp.lan>
2018-01-12 11:06:15 -0500

    took out try/except blocks, checked the negation of all results from dao before return

--- model.py
+++ model.py
@@ -1,43 +1,33 @@
-from dao import Dao
-import sys
+import Dao
 class Model:
   
     def __init__(self, collection):
-        self._id = str(collection['_id'])
-        self.name = collection['name']
-        
+        self._id = str(collection['_id']) if '_id' in collection else None
+        self.name = collection['name']      
 
     @staticmethod
     def get_by_id(student_Id):
         dao = Dao()
-        try:
-          student = dao.get_by_id(student_Id)
-        except:
-          return None
-        if student:
-            
-            return Model(student)
-        else:
+        student = dao.get_by_id(student_Id)
+        if not student:
             return None
-        
+        return Model(student)
+           
     @staticmethod
     def addStudent(studentInfo):
         dao = Dao()
-        try:
-          new= dao.addStudent(studentInfo)
-          return Model(new)
-        except:
+        new= dao.addStudent(studentInfo)
+        if not new:
           return None
-        
+        return Model(new)
 
     @staticmethod
     def update(student_Id, studentInfo):
         dao = Dao()
-        try:
-          updated = dao.update(student_Id, studentInfo)
-          return Model(updated)
-        except:
+        updated = dao.update(student_Id, studentInfo)
+        if not updated:
           return None
+        return Model(updated)
 
     @staticmethod
     def deleteAll():
@@ -48,23 +38,15 @@ class Model:
     @staticmethod
     def delete(student_Id):
         dao = Dao()
-        try:
-          deleted=dao.delete(student_Id)
-          return Model(deleted)
-        except:    
-          return None
+        deleted=dao.delete(student_Id)
+        return deleted
 
     @staticmethod
     def getStudents():
         dao = Dao()
         allstud = dao.getStudents()
         students = []
-        for s in allstud.find():
-            print (s,file=sys.stderr)
-            students.append(repr(Model(s)))
-        return str(students)
-
-   
-
-
+        for s in allstud:
+            students.append(Model(s))
+        return students
 
