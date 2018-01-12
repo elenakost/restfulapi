b6e116b91484f974bfad051cf3a84c3a0bc35756
ElenaKostandin <elenakostandin@admins-mbp.lan>
2018-01-12 10:43:57 -0500

    added validator.py, changed status codes, added jsonify

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Resource, Api, reqparse, request, abort
 from model import Model
-from voluptuous.schema_builder import Schema, Required
-from voluptuous.validators import Length, All, MultipleInvalid
+from validator import *
 import json
 app = Flask(__name__)
 api = Api(app)
@@ -10,76 +9,52 @@ api = Api(app)
 parser = reqparse.RequestParser()
 parser.add_argument('name')
 
-idSchema = Schema(All(str, Length(min=24, max=24)))
-jsSchema=Schema(str)
-
 class Student(Resource):
     def get(self, student_id):
         try:
-            if(idSchema(student_id)==student_id):
-                student=Model.get_by_id(student_id)
-            else:
-                raise AssertionError('MultipleInvalid not raised')
+            validator.validateId(student_id)
+            student=Model.get_by_id(student_id) 
         except MultipleInvalid as e:
             exc = e
             print (str(exc))
-            return '', 406
+            return '', 400
         except:
            return 'error', 500
-        if student:         
-            return json.dumps(student.__dict__)
-        abort(404, message="Getting Student {} failed".format(student_id))
+        if not student:
+            abort(404, message="Getting Student {} failed".format(student_id))
+        _return=jsonify(student.__dict__)
+        _return.status_code=200
+        return _return      
+        
         
     def delete(self, student_id):
         try:
-            if(idSchema(student_id)==student_id):
-                print("app begin")
-                deleted = Model.delete(student_id)
-                print("delete end")
-            else:
-                raise AssertionError('MultipleInvalid not raised')           
+            validator.validateId(student_id)
+            deleted = Model.delete(student_id)          
         except MultipleInvalid as e:
             exc = e
-            print (str(exc))
-            return '', 406
+            return (str(exc)), 400
         except:
             return '', 500
-        if deleted:
-            return '', 204
-        abort(404, message="Deleting Student {} failed".format(student_id))
+        if not deleted:
+            abort(404, message="Deleting Student {} failed".format(student_id))
+        return '', 204
         
-        
-    def post(self):
-        args = parser.parse_args()
-        try:
-            if(jsSchema(args['name'])==args['name']):
-                n= {'name': args['name']}
-                student = Model.addStudent(n)
-            else:
-                raise AssertionError('MultipleInvalid not raised')           
-        except MultipleInvalid as e:
-            exc = e
-            print (str(exc))
-            return '', 406
-        except:
-            return '', 500
-        return json.dumps(student.__dict__), 201
         
     def put(self, student_id):
         args = parser.parse_args()
         try:
-            if(jsSchema(args['name'])==args['name'] and idSchema(student_id)==student_id):
-                n= {'name': args['name']}
-                student = Model.update(student_id, n)
-            else:
-                raise AssertionError('MultipleInvalid not raised')           
+            validator.validatePut(student_id, args['name'])
+            n= {'name': args['name']}
+            student = Model.update(student_id, n)          
         except MultipleInvalid as e:
             exc = e
-            print (str(exc))
-            return '', 406
+            return (str(exc)), 400
         except:
             return '', 500
-        return json.dumps(student.__dict__), 201
+        _return=jsonify(student.__dict__)
+        _return.status_code=201
+        return _return    
     
 class StudentList(Resource):
     def get(self):
@@ -87,38 +62,40 @@ class StudentList(Resource):
             students = Model.getStudents() 
         except:
             return 'an error',500
-        if students:
-            return students, 200
-        abort(404, message="Getting all Students failed")
+        if not students:
+            abort(404, message="Getting all Students failed")
+        jsonlist=[]
+        for s in students:
+            jsonlist.append(s.__dict__)
+        return jsonlist,200
         
         
     def post(self):
         args = parser.parse_args()
         try:
-            print(args['name'])
-            if(jsSchema(args['name'])==args['name']):
-                n= {'name': args['name']}
-                student = Model.addStudent(n)
-            else:
-                raise AssertionError('MultipleInvalid not raised')           
+            validator.validateData(args['name'])
+            n= {'name': args['name']}
+            student = Model.addStudent(n)        
         except MultipleInvalid as e:
             exc = e
-            print (str(exc))
-            return '', 406
+            return (str(exc)), 400
         except:
             return '', 500
-        return json.dumps(student.__dict__), 201
+        _return=jsonify(student.__dict__)
+        _return.status_code=201
+        return _return    
 
     def delete(self):
         try:
             deleted = Model.deleteAll()
         except:
             return '', 500
-        return '', 204
+        return 'deleted', 204
 
 api.add_resource(StudentList, '/students')
 api.add_resource(Student, '/students/<student_id>')
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
 
