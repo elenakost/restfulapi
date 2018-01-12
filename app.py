af9d5c9fcf7a6c867a3d8e15ca952efe9daa918f
elenakost <elenakostandin@gmail.com>
2018-01-12 12:52:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -89,4 +89,4 @@ api.add_resource(StudentList, '/students')
 api.add_resource(Student, '/students/<student_id>')
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port=5000, debug = True)
+    app.run(host = '0.0.0.0', port = 5000, debug = True)
